93ff18a120b69846ff523d1f0847c6e96f660890
diced <git@diced.sh>
2026-03-28 23:57:51 -0700

    fix: reformat routes to include catch-all

--- src/client/routes.tsx
+++ src/client/routes.tsx
@@ -8,6 +8,11 @@ import FourOhFour from './pages/404';
 import Login from './pages/auth/login';
 import Root from './Root';
 
+const fourOhFourCatchall = {
+  path: '*',
+  Component: FourOhFour,
+};
+
 export async function dashboardLoader() {
   try {
     const res = await fetch('/api/server/settings/web');
@@ -33,17 +38,16 @@ export const router = createBrowserRouter([
       {
         ErrorBoundary: RootErrorBoundary,
         children: [
-          { path: '*', Component: FourOhFour },
+          fourOhFourCatchall,
           {
-            path: '/auth',
             children: [
-              { path: 'login', Component: Login },
-              { path: 'register', lazy: () => import('./pages/auth/register') },
+              { path: 'auth/login', Component: Login },
+              { path: 'auth/register', lazy: () => import('./pages/auth/register') },
               {
-                path: 'setup',
+                path: 'auth/setup',
                 lazy: () => import('./pages/auth/setup'),
               },
-              { path: 'tos', lazy: () => import('./pages/auth/tos') },
+              { path: 'auth/tos', lazy: () => import('./pages/auth/tos') },
             ],
           },
           {
@@ -60,37 +64,26 @@ export const router = createBrowserRouter([
                   { path: 'files', lazy: () => import('./pages/dashboard/files') },
                   { path: 'folders/*', lazy: () => import('./pages/dashboard/folders') },
                   { path: 'urls', lazy: () => import('./pages/dashboard/urls') },
+                  { path: 'upload/file', lazy: () => import('./pages/dashboard/upload/file') },
+                  { path: 'upload/text', lazy: () => import('./pages/dashboard/upload/text') },
+
+                  // admin routes
                   {
-                    path: 'upload',
-                    children: [
-                      { path: 'file', lazy: () => import('./pages/dashboard/upload/file') },
-                      { path: 'text', lazy: () => import('./pages/dashboard/upload/text') },
-                    ],
-                  },
-                  {
-                    path: 'admin',
                     loader: async () => {
                       const res = await fetch('/api/user');
-                      if (!res.ok) {
-                        return redirect('/auth/login');
-                      }
+                      if (!res.ok) return redirect('/auth/login');
 
                       const { user } = await res.json();
                       if (!isAdministrator(user.role)) return redirect('/dashboard');
                     },
                     children: [
-                      { path: 'invites', lazy: () => import('./pages/dashboard/admin/invites') },
-                      { path: 'settings', lazy: () => import('./pages/dashboard/admin/settings') },
-                      { path: 'actions', lazy: () => import('./pages/dashboard/admin/actions') },
+                      { path: 'admin/invites', lazy: () => import('./pages/dashboard/admin/invites') },
+                      { path: 'admin/settings', lazy: () => import('./pages/dashboard/admin/settings') },
+                      { path: 'admin/actions', lazy: () => import('./pages/dashboard/admin/actions') },
+                      { path: 'admin/users', lazy: () => import('./pages/dashboard/admin/users') },
                       {
-                        path: 'users',
-                        children: [
-                          { index: true, lazy: () => import('./pages/dashboard/admin/users') },
-                          {
-                            path: ':id/files',
-                            lazy: () => import('./pages/dashboard/admin/users/[id]/files'),
-                          },
-                        ],
+                        path: 'admin/users/:id/files',
+                        lazy: () => import('./pages/dashboard/admin/users/[id]/files'),
                       },
                     ],
                   },
